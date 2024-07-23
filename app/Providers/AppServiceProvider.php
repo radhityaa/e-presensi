e332966ca1240408bc64a8af48392a54e92f050a
Rama Adhitya Setiadi <ramaadhityasetiadi002@gmail.com>
2024-07-23 09:43:03 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
+
         Paginator::useBootstrapFive();
         Gate::before(function ($user, $ability) {
             return $user->hasRole('admin') ? true : null;
